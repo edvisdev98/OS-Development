84449d3da8a7e756f92289ce84471621ef8a4d2b
rok3tt <bzsolczay@gmail.com>
2025-08-07 10:37:51 +1000

    refactor(bootloader): swapped tabs to spaces in 'core/entry.asm'

--- bootloader/core/entry.asm
+++ bootloader/core/entry.asm
@@ -1,19 +1,20 @@
-	bits 16
-
-	section .data
+    bits 16
+    
+    section .data
 msg_hello: db 'Hello from stage 1.5!', 0x0A, 0x0D, 0
-	
-	section .text
-	global _start
+    	
+    section .text
+    global _start
 _start:
-	;; Print a little message to prove that we have
-	;; in fact loaded into the core.
-	mov si, msg_hello
-	call printr
+    ;; Print a little message to prove that we have
+    ;; in fact loaded into the core.
+    mov si, msg_hello
+    call printr
 .L1:
-	hlt
-	cli
-
-	jmp .L1
-
+    hlt
+    cli
+    
+    jmp .L1
+    
 %include "../boot/vgabios.asm"
+    
